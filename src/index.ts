5a23c53788781defe9b2386299928cfc0f90eb9a
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-03 16:44:15 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ app.use(cookieParser());
 
 connectToDBs();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 export const clientDomain =
   process.env.NODE_ENV === "development"
