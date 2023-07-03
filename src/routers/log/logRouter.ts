418f6395382a2cd53894c9b3d4fd0352002a66b7
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-03 16:54:08 +0300

    asd

--- src/routers/log/logRouter.ts
+++ src/routers/log/logRouter.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { getSignInReqModel } from "../../oc-models/auth/signinReqModel";
 import { getInvalidPromptModel } from "../../oc-models/data/prompts/invalidPromptModel";
+import { getRestRequestModel } from "../../oc-models/restRequestModel";
 
 const router = express.Router();
+
+router.post("/logReq", async (req, res) => {
+  const restRequestModel = getRestRequestModel();
+  await new restRequestModel({ ...req.body }).save();
+  return res.status(200).send();
+});
 
 router.post("/logSignin", async (req, res) => {
   const signInReqModel = getSignInReqModel();
