4ee9850ed28b35c471a87343fc203784c7c76322
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-06 09:45:13 +0300

    Update readRouter.ts

--- src/routers/read/readRouter.ts
+++ src/routers/read/readRouter.ts
@@ -2,6 +2,12 @@ import express from "express";
 import { getSignInReqModel } from "../../oc-models/auth/signinReqModel";
 import { getInvalidPromptModel } from "../../oc-models/data/prompts/invalidPromptModel";
 import { getPromptPriceModel } from "../../oc-models/data/prompts/promptPriceModel";
+import { getOpenAICallModel } from "../../oc-models/data/prompts/openAICallModel";
+import { OCModels } from "@failean/shared-types";
+import { CreateChatCompletionResponse } from "openai";
+import { getUserModel } from "../../mongo-models/auth/userModel";
+import { getTokenModel } from "../../mongo-models/accounts/tokenModel";
+import { getIdeaModel } from "../../mongo-models/data/ideas/ideaModel";
 
 const router = express.Router();
 
@@ -14,33 +20,93 @@ router.get("/usersWhoLoggedInLastDay", async (_, res) => {
   return res.status(200).json({ total: lastDay.length, details: lastDay });
 });
 
-router.get("/invalidPromptEvents", async (_, res) => {
-  const invalidPromptModel = getInvalidPromptModel();
-  return res.status(200).json({ events: await invalidPromptModel.find() });
-});
+router.get("/invalidPromptEvents", async (_, res) =>
+  res.status(200).json({ events: await getInvalidPromptModel().find() })
+);
 
-router.post("/getAVGPriceForPrompt", async (req, res) => {
-  const { promptName } = req.body;
-  const promptPriceModel = getPromptPriceModel();
-  const prices = await promptPriceModel.find({ promptName });
+router.post("/avgPriceForPrompt", async (req, res) => {
+  const prices = await getPromptPriceModel().find({ avg: req.body.promptName });
   let avg = 0;
   prices.forEach(({ priceInOpenAITokensForAVG }) => {
     avg += priceInOpenAITokensForAVG;
   });
   return res.status(200).json({ avg });
 });
-/*
-כמות הטוקנים ששילמנו לopen ai
-כמות התוקנים שהשתמשו לנו
-כל המידע על יוזרים 
-כל המידע על תוקנים 
+
+router.get("/numberOfOpenAITokensWeUsed", async (_, res) => {
+  const openAICallModel = getOpenAICallModel();
+  const total = await openAICallModel.find();
+  const completions: CreateChatCompletionResponse[] = total.map(
+    (doc): OCModels.Data.Prompts.OpenAICall =>
+      JSON.parse(doc.stringifiedCompletion)
+  ) as any;
+  let sum = 0;
+  completions.forEach(
+    (completion) => (sum += completion.usage?.total_tokens || 0)
+  );
+  return res.status(200).json({ sum });
+});
+
+router.get("/users", async (_, res) =>
+  res.status(200).json({ users: await getUserModel().find() })
+);
+
+router.get("/tokens", async (_, res) =>
+  res.status(200).json({ tokens: await getTokenModel().find() })
+);
+
+router.get("/ideas", async (_, res) =>
+  res.status(200).json({ ideas: await getIdeaModel().find() })
+);
+
+router.get("/avgIdeasPerUser", async (_, res) =>
+  res.status(200).json({
+    avg:
+      (await getIdeaModel().find()).length /
+      (await getUserModel().find()).length,
+  })
+);
+
+router.get("/userWithMostIdeas", async (_, res) => {
+  const ideas = await getIdeaModel().find();
+  const users = await getUserModel().find();
+  let max = 0;
+  let userID = "";
+  users.forEach(({ _id }) => {
+    const hisIdeas = ideas.filter(({ owner }) => owner === _id);
+    if (hisIdeas.length > max) {
+      max = hisIdeas.length;
+      userID = _id;
+    }
+  });
+  return res
+    .status(200)
+    .json({ max, user: users.find(({ _id }) => _id === userID) });
+});
+
+router.get("/usersWithZeroIdeas", async (_, res) => {
+  const ideas = await getIdeaModel().find();
+  return res.status(200).json({
+    usersWithZero: (await getUserModel().find()).filter(
+      ({ _id }) => ideas.filter(({ owner }) => owner === _id).length === 0
+    ),
+  });
+});
+
+router.get("/usersWithOneIdeas", async (_, res) => {
+  const ideas = await getIdeaModel().find();
+  return res.status(200).json({
+    usersWithOne: (await getUserModel().find()).filter(
+      ({ _id }) => ideas.filter(({ owner }) => owner === _id).length === 1
+    ),
+  });
+});
+
+/*כמות התוקנים שהשתמשו לנו
 זמינות שרת אחוזי שמישות 
 לחיצות על הסייד בר
 התראות על יוזרים שלא השתמשו 72 שעות 
 טעות בהזנת סיסמא יותר מ3 פעמים בשעה
-כמות הרעיונות 
-כמות הרעיונות הממוצע ליוזר
-כמות רעיונות מקסימלית ליוזר 
 כמות שאנשים שהריצו את
 כמות השינויים 
 לתעד כל run all
