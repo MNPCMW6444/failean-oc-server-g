740cdc66a81bbd674b7f77caed61efb4f0a2e66c
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-09 14:44:23 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import basicAuth from "express-basic-auth";
 import logRouter from "./routers/log/logRouter";
 import readRouter from "./routers/read/readRouter";
 import { connectToDBs } from "./dbConnection";
+import { getExpressRequestModel } from "./oc-models/expressRequestModel";
 
 dotenv.config();
 
@@ -13,7 +14,38 @@ const app = express();
 const port = process.env.PORT || 6777;
 app.use(cookieParser());
 
-connectToDBs();
+connectToDBs() /* .then(() => {
+  try {
+    let startDate = new Date("2023-07-08T15:00:00.000Z");
+    let endDate = new Date("2023-07-08T16:00:00.000Z");
+
+    getExpressRequestModel()
+      .find({
+        createdAt: {
+          $gte: startDate,
+          $lt: endDate,
+        },
+      })
+      .then((x) => {
+        x.map(({ stringifiedReq }) => {
+          let requestObj = JSON.parse(stringifiedReq);
+
+          let xForwardedFor = requestObj.headers["x-forwarded-for"];
+
+          let ipAddresses = xForwardedFor.split(", ");
+
+          let clientIP = ipAddresses[0];
+
+          console.log(clientIP);
+
+          //console.log(JSON.parse(stringifiedReq));
+        });
+        console.log("finished");
+      });
+  } catch (e) {
+    console.log(e);
+  }
+}) */;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
