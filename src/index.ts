2047803a7384959bcf3cb057c8aa9774d8f3dd58
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-13 17:21:37 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import basicAuth from "express-basic-auth";
 import logRouter from "./routers/log/logRouter";
 import readRouter from "./routers/read/readRouter";
 import { connectToDBs } from "./dbConnection";
-import { getExpressRequestModel } from "./oc-models/expressRequestModel";
 
 dotenv.config();
 
@@ -14,38 +13,7 @@ const app = express();
 const port = process.env.PORT || 6777;
 app.use(cookieParser());
 
-connectToDBs() /* .then(() => {
-  try {
-    let startDate = new Date("2023-07-08T15:00:00.000Z");
-    let endDate = new Date("2023-07-08T16:00:00.000Z");
-
-    getExpressRequestModel()
-      .find({
-        createdAt: {
-          $gte: startDate,
-          $lt: endDate,
-        },
-      })
-      .then((x) => {
-        x.map(({ stringifiedReq }) => {
-          let requestObj = JSON.parse(stringifiedReq);
-
-          let xForwardedFor = requestObj.headers["x-forwarded-for"];
-
-          let ipAddresses = xForwardedFor.split(", ");
-
-          let clientIP = ipAddresses[0];
-
-          console.log(clientIP);
-
-          //console.log(JSON.parse(stringifiedReq));
-        });
-        console.log("finished");
-      });
-  } catch (e) {
-    console.log(e);
-  }
-}) */;
+connectToDBs();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -53,21 +21,27 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 export const ocClientDomain =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5998"
-    : "https://oc.failean.com";
+    : `https://${
+        process.env.WHITE_ENV === "dev" ? "tst" : process.env.WHITE_ENV
+      }oc.failean.com`;
 
 export const clientDomain =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5999"
-    : "https://dev.failean.com";
+    : `https://${
+        process.env.WHITE_ENV
+          ? process.env.WHITE_ENV + "."
+          : process.env.WHITE_ENV
+      }.failean.com`;
 
-export const ocServerDomain =
+export const mainServerDomain =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:6777"
-    : "https://tstocserver.failean.com";
+    ? "http://localhost:6555"
+    : `https://${process.env.WHITE_ENV}mainserver.failean.com`;
 
 app.use(
   cors({
-    origin: [clientDomain, ocClientDomain, ocServerDomain],
+    origin: [ocClientDomain, clientDomain, mainServerDomain],
     credentials: true,
   })
 );
