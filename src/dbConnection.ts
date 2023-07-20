294daeb6edc237dd078b6d8301175525ab136eec
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-20 13:27:29 +0300

    modified:   src/dbConnection.ts

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -5,15 +5,23 @@ export let safeDB: mongoose.Connection | null = null;
 
 export const connectToDBs = async () => {
   try {
-    ocDB = await mongoose.createConnection("" + process.env.OC, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions);
+    ocDB = await mongoose.createConnection(
+      `mongodb://${process.env.OC_USER}:${process.env.OC_PASS}@${process.env.OC_HOST}:${process.env.OC_PORT}/x?ssl=true`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true,
+      } as ConnectOptions
+    );
 
-    safeDB = await mongoose.createConnection("" + process.env.SAFE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as ConnectOptions);
+    safeDB = await mongoose.createConnection(
+      `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/x?ssl=true`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true,
+      } as ConnectOptions
+    );
 
     console.log("connected to main-mongo and oc-mongo");
   } catch (e) {
