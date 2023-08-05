c3ffbe8bf4cdd04fe1c6a2f990c91b0919966305
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-05 16:58:12 +0300

    wd

--- src/index.ts
+++ src/index.ts
@@ -46,13 +46,6 @@ app.use(
   })
 );
 
-app.use(
-  basicAuth({
-    users: { client: process.env.OCPASS + "xx" },
-    challenge: true,
-  })
-);
-
 app.listen(port as any, "0.0.0.0", () =>
   console.log(`Server started on port: ${port}`)
 );
